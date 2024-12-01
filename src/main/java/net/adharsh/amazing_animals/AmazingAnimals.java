ec3d1bcdecda63cf08f0a50255dec1954bcb11b8
unknowncoder-gamer <rajithaanu1985@gmail.com>
2024-12-01 22:42:31 +0530

    The first commit of Amazing animals mod

---
+++ src/main/java/net/adharsh/amazing_animals/AmazingAnimals.java
@@ -0,0 +1,15 @@
+package net.adharsh.amazing_animals;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AmazingAnimals implements ModInitializer {
+	public static final String MOD_ID = "amazing-animals";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/adharsh/amazing_animals/AmazingAnimalsClient.java
@@ -0,0 +1,10 @@
+package net.adharsh.amazing_animals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AmazingAnimalsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
